0d5d5acc0e35be7ba7cd5ca24bbabfd1c100159f
tanya-kostecki <tanya.pavlova1294@gmail.com>
2023-09-13 12:58:43 +0200

    map и подключение верстки

---
+++ my-app/src/api.js
@@ -0,0 +1,5 @@
+export async function getAllTracks() {
+    const response = await fetch('https://skypro-music-api.skyeng.tech/catalog/track/all/')
+    const data = await response.json()
+    return data
+}
--- my-app/src/components/playlist/playlist-item-component.jsx
+++ my-app/src/components/playlist/playlist-item-component.jsx
@@ -1,42 +1,60 @@
 import React from 'react'
 import * as S from './playlist.styles'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { getAllTracks } from '../../api'
+import { TRACKS } from './constants'
 
-export function PlaylistItem({ track }) {
+export function PlaylistItem() {
+  const [tracks, setTracks] = useState()
+
+  useEffect(() => {
+    getAllTracks().then((tracks) => {
+      setTracks(tracks)
+    })
+  }, [])
+
+
   return (
-    <S.PlaylistItem>
-      <S.PlaylistTrack>
-        <S.TrackTitle>
-          <S.TrackTitleImage>
-            <S.TrackTitleSvg alt="music">
-              <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-            </S.TrackTitleSvg>
-          </S.TrackTitleImage>
-          <div className="track__title-text">
-            <S.TrackTitleLink href={track.titleLink}>
-              {track.title}
-              <S.TrackTitleSpan>{track.titleSpan}</S.TrackTitleSpan>
-            </S.TrackTitleLink>
-          </div>
-        </S.TrackTitle>
-        <S.TrackAuthor>
-          <S.TrackAuthorLink href={track.authorLink}>
-            {track.author}
-          </S.TrackAuthorLink>
-        </S.TrackAuthor>
-        <S.TrackAlbum>
-          <S.TrackAlbumLink href={track.albumLink}>
-            {track.album}
-          </S.TrackAlbumLink>
-        </S.TrackAlbum>
-        <div className="track__time">
-          <S.TrackTimeSvg alt="time">
-            <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-          </S.TrackTimeSvg>
-          <S.TrackTimeText className="track__time-text">
-            {track.time}
-          </S.TrackTimeText>
-        </div>
-      </S.PlaylistTrack>
-    </S.PlaylistItem>
+    <>
+      {tracks?.map((track) => (
+          <S.PlaylistItem key={track.id}>
+            <S.PlaylistTrack >
+              <S.TrackTitle>
+                <S.TrackTitleImage>
+                  <S.TrackTitleSvg alt="music">
+                    <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                  </S.TrackTitleSvg>
+                </S.TrackTitleImage>
+                <div className="track__title-text">
+                  <S.TrackTitleLink href={track.track_file}>
+                    {track.name}
+                    <S.TrackTitleSpan>{track.titleSpan}</S.TrackTitleSpan>
+                  </S.TrackTitleLink>
+                </div>
+              </S.TrackTitle >
+              <S.TrackAuthor>
+                <S.TrackAuthorLink href={track.authorLink}>
+                  {track.author}
+                </S.TrackAuthorLink>
+              </S.TrackAuthor>
+              <S.TrackAlbum>
+                <S.TrackAlbumLink href={track.albumLink}>
+                  {track.album}
+                </S.TrackAlbumLink>
+              </S.TrackAlbum>
+              <div className="track__time">
+                <S.TrackTimeSvg alt="time">
+                  <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+                </S.TrackTimeSvg>
+                <S.TrackTimeText className="track__time-text">
+                  {track.duration_in_seconds}
+                </S.TrackTimeText>
+              </div>
+            </S.PlaylistTrack>
+          </S.PlaylistItem>
+        )
+      )}
+    </>
   )
 }
